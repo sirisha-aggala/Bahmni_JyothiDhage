d634ba038a126203fc97ec56ff7aa41e6059ba9a
Admin <Admin@DESKTOP-QC3B84H>
2024-02-08 22:39:07 +0000

    Admin task done

--- src/test/java/stepDefinitions/AdminCSVExportStepDefinition.java
+++ src/test/java/stepDefinitions/AdminCSVExportStepDefinition.java
@@ -1,5 +1,30 @@
 package stepDefinitions;
 
-public class AdminCSVExportStepDefinition {
+import appPages.AdminpageCSVExport;
+import appPages.HomePage;
+import appPages.RegistrationPage;
+import drivers.DriverManager;
+import io.cucumber.java.en.Then;
 
+public class AdminCSVExportStepDefinition {
+	HomePage homepage =new HomePage(DriverManager.getDriver());
+	   AdminpageCSVExport CSVExportpage = new AdminpageCSVExport();
+	   @Then("user click on the Admin Module")
+		public void user_click_on_the_Admin_Module() throws InterruptedException {
+		homepage.navigateToAdminScreen();
+			   }
+	   @Then("user select the CSV Export")
+	   public void user_select_the_CSV_Export() throws InterruptedException {
+		   CSVExportpage.navigatetoCSVExportpage();
+	   }
+	  
+	   @Then("user select the file and click on Export button")
+	   public void user_select_the_file_and_click_on_Export_button() throws InterruptedException {
+		   CSVExportpage.CSVExportfile();
+	   }
+	   
+	   @Then("user should able to download the file")
+	   public void user_should_able_to_download_the_file() {
+		   
+	   }
 }
--- src/test/java/stepDefinitions/AdminCSVUploadStepDefinition.java
+++ src/test/java/stepDefinitions/AdminCSVUploadStepDefinition.java
@@ -1,10 +1,28 @@
 package stepDefinitions;
 
+import appPages.AdminpageCSVUpload;
 import appPages.HomePage;
-import appPages.RegistrationPage;
 import drivers.DriverManager;
+import io.cucumber.java.en.Then;
 
 public class AdminCSVUploadStepDefinition {
 	 HomePage homepage =new HomePage(DriverManager.getDriver());
-	 RegistrationPage registrationPage = new RegistrationPage(DriverManager.getDriver());
+	AdminpageCSVUpload CSVUpload = new AdminpageCSVUpload(DriverManager.getDriver());
+	@Then("user click on the Admin Module")
+		public void user_click_on_the_Admin_Module() throws InterruptedException {
+		homepage.navigateToAdminScreen();
+	}
+	@Then("user select the CSV Upload")
+	 public void user_select_the_CSV_Upload() throws InterruptedException {
+		CSVUpload.navigatetoCSVUploadpage();
+	}
+	@Then("User select file type")
+	public void User_select_file_type() {
+		CSVUpload.CSVUploadfile();
+	}
+	
+	@Then("user should able to upload the file ")
+	public void user_should_able_to_upload_the_file () {
+		
+	}
 }
